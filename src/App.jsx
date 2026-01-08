81b63e00a89dbd95833a6c8b7e526be9042c37d3
Khanh <quckhnh1412@gmail.com>
2026-01-08 22:46:04 +0700

    Convert src from submodule to normal folder

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { ScrollTrigger,SplitText } from "gsap/all"
+import gsap from "gsap"
+import NavBar from "./components/NavBar"
+import Hero from "./components/Hero"
+
+gsap.registerPlugin(ScrollTrigger,SplitText)
+const App = () => {
+  return (
+   <main>
+    <NavBar></NavBar>
+    <Hero/>
+    <div className="h-dvh bg-black"></div>
+   </main>
+  )
+}
+
+export default App
